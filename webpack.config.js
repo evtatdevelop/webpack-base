47421083c76494ac6eeab986f039e018508a0b9a
Evgenii <evtatdevelop@gmail.com>
2020-09-30 10:14:00 +0700

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -20,5 +20,19 @@ module.exports = {
   ],
   resolve: {
     extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
